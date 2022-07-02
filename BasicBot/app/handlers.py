ae5d3f1763199dedf3434a749e71db54a31b207b
AltF4mx <youwatchma@gmail.com>
2022-07-02 16:58:16 +0700

    Теперь бот представляется.

--- BasicBot/app/handlers.py
+++ BasicBot/app/handlers.py
@@ -6,3 +6,5 @@ from app import bot
 async def on_join(event: events.ChatAction.Event):
     if event.is_group and event.user_added and event.user_id == bot.me.id:
         await bot.send_message(event.chat.id, 'Всем привет!')
+        await bot.send_message(event.chat.id, 'Я ' + bot.me.first_name)
+        await bot.send_message(event.chat.id, 'Я послежу тут за вами немного ;)')
