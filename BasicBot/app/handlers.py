d4265337c38e44efe20b5953bf3e23cea7cce7eb
AltF4mx <youwatchma@gmail.com>
2022-07-02 20:35:23 +0700

    Добавлено приветствие нового участника.

--- BasicBot/app/handlers.py
+++ BasicBot/app/handlers.py
@@ -8,3 +8,7 @@ async def on_join(event: events.ChatAction.Event):
         await bot.send_message(event.chat.id, 'Всем привет!')
         await bot.send_message(event.chat.id, 'Я ' + bot.me.first_name)
         await bot.send_message(event.chat.id, 'Я послежу тут за вами немного ;)')
+        
+@bot.on(events.ChatAction(func=lambda e: (e.user_added or e.user_joined) and e.user_id != bot.me.id))
+async def greet(event: events.ChatAction.Event):
+    await event.respond('Привет, ' + event.user.first_name + ', веди себя хорошо!')
