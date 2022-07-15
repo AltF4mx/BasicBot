e04ba6a74d9905dfc6da7fc59ab18f35462f2cdd
AltF4mx <youwatchma@gmail.com>
2022-07-15 22:56:15 +0700

    Добавлена модель таблицы с плохими словами, изменены имена таблиц (исправлена ошибка)

--- BasicBot/app/models.py
+++ BasicBot/app/models.py
@@ -2,14 +2,27 @@ from tortoise.models import Model
 from tortoise import fields
 
 class Chat(Model):
-    tablename = 'chats'
     id = fields.BigIntField(pk=True)
     last_admins_update = fields.DatetimeField(null=True)
     
+    class Meta:
+        table = 'chats'
+    
+    
 class ChatMember(Model):
-    tablename = 'chat_members'
     id = fields.IntField(pk=True)
     user_id = fields.BigIntField()
     chat_id = fields.BigIntField()
     is_admin = fields.BooleanField(default=False)
     warns = fields.IntField(default=0)
+    
+    class Meta:
+        table = 'chat_members'
+    
+    
+class Slang(Model):
+    id = fields.IntField(pk=True)
+    word = fields.CharField(max_length=255, unique=True)
+    
+    class Meta:
+        table = 'slang'
